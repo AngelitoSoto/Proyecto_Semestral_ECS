b2a9c4ccc0daef820687e5431a645914036d99d9
AngelitoSoto <ange.sotob@duocuc.cl>
2026-05-28 10:12:22 -0400

    Refactor FormDespacho to update delivery status

--- front_despacho/src/componentes/CrudAdmin/FormDespacho.jsx
+++ front_despacho/src/componentes/CrudAdmin/FormDespacho.jsx
@@ -3,15 +3,18 @@ import Swal from "sweetalert2";
 import axios from "axios";
 
 export const FormDespacho = ({ venta, onClose }) => {
+
   const { register, handleSubmit } = useForm();
 
   const onSubmit = async (data) => {
+
     console.log("onSubmit ejecutado");
+
     const jsonData = {
       fechaDespacho: data.fechaDespacho,
       patenteCamion: data.patenteCamion,
       intento: 0,
-      entregado: false,
+      despachado: false,
       idCompra: venta.idVenta,
       direccionCompra: venta.direccionCompra,
       valorCompra: venta.valorCompra,
@@ -24,49 +27,70 @@ export const FormDespacho = ({ venta, onClose }) => {
     console.log("Datos del formulario:", jsonData);
 
     try {
-      console.log("DESPACHO:", despacho);
-      console.log("ID:", despacho?.idDespacho);
+
       console.log("API DESPACHOS:", import.meta.env.VITE_API_DESPACHOS);
-      console.log("URL:", `${import.meta.env.VITE_API_DESPACHOS}/${despacho.idDespacho}`);
+
       await axios.put(
         `${import.meta.env.VITE_API_VENTAS}/${venta.idVenta}`,
         jsonDataSales,
         {
-          headers:{
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-      }
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
         }
       );
 
-      await axios.post(import.meta.env.VITE_API_DESPACHOS, jsonData, {
-        headers:{
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-    }
-      });
+      await axios.post(
+        import.meta.env.VITE_API_DESPACHOS,
+        jsonData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        }
+      );
+
       Swal.fire({
         title: "Despacho registrado 🛻!",
         text: "El despacho ha sido generado con éxito en la base de datos",
         icon: "success",
         confirmButtonText: "Aceptar",
       });
+
     } catch (error) {
+
       console.error("Error en la solicitud:", error);
+
+      Swal.fire({
+        title: "Error",
+        text: "No se pudo generar el despacho",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+
     }
+
     onClose();
   };
+
   return (
     <>
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col justify-center text-center px-24 text-xl"
       >
+
         <div className="mx-auto text-3xl font-bold mb-10 text-teal-600">
           Ingreso de orden de despacho
         </div>
+
         <div className="mb-5">
-          <label className="block font-bold mb-2">Fecha de despacho</label>
+          <label className="block font-bold mb-2">
+            Fecha de despacho
+          </label>
+
           <input
             type="date"
             placeholder="Ingresa fecha de despacho"
@@ -74,8 +98,12 @@ export const FormDespacho = ({ venta, onClose }) => {
             {...register("fechaDespacho", { required: true })}
           />
         </div>
+
         <div className="mb-5">
-          <label className="block font-bold mb-2">Patente de camión</label>
+          <label className="block font-bold mb-2">
+            Patente de camión
+          </label>
+
           <input
             type="text"
             placeholder="Elige patente de camión"
@@ -83,10 +111,12 @@ export const FormDespacho = ({ venta, onClose }) => {
             {...register("patenteCamion", { required: true })}
           />
         </div>
+
         <div className="mb-5">
           <label className="block font-bold mb-2">
             Orden de compra asociado
           </label>
+
           <input
             type="number"
             disabled={true}
@@ -94,8 +124,12 @@ export const FormDespacho = ({ venta, onClose }) => {
             className="border border-gray-300 rounded-lg block w-full text-slate-400 p-1"
           />
         </div>
+
         <div className="mb-5">
-          <label className="block font-bold mb-2">Dirección de entrega</label>
+          <label className="block font-bold mb-2">
+            Dirección de entrega
+          </label>
+
           <input
             type="text"
             disabled={true}
@@ -103,8 +137,12 @@ export const FormDespacho = ({ venta, onClose }) => {
             className="border border-gray-300 rounded-lg block w-full text-slate-400 p-1"
           />
         </div>
+
         <div className="mb-5">
-          <label className="block font-bold mb-2">Valor de compra</label>
+          <label className="block font-bold mb-2">
+            Valor de compra
+          </label>
+
           <input
             type="number"
             value={venta.valorCompra}
@@ -119,6 +157,7 @@ export const FormDespacho = ({ venta, onClose }) => {
         >
           Asignar despacho
         </button>
+
       </form>
     </>
   );
