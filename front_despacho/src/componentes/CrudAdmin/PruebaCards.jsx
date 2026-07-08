96371ed42aca74422fdef50f38bd39175baa0796
AngelitoSoto <ange.sotob@duocuc.cl>
2026-07-08 16:47:02 -0400

    Change Card title from 'Ofertas' to 'Ordenes de Ventas'

--- front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
+++ front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,7 +11,7 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="Ofertas"
+          title="Ordenes de Ventas"
           description="Revisa las últimas oc realizadas para generar su despacho"
           buttonText="Consultar"
           onClick={() => {
